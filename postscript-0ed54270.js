de0fc70683b5de0705a2e7237ddf641b7a60d94b
railscraft-gh <railscraft-gh@users.noreply.github.com>
2026-07-21 14:29:19 +0000

    deploy: 0e76dfec1bfe111c4a67b80d9cbef219a670b39b

---
+++ postscript-0ed54270.js
@@ -0,0 +1,10 @@
+await Promise.all([
+  import("./static/scripts/script-0-ec880d15.js"),
+  import("./static/scripts/script-1-1f9394e6.js"),
+  import("./static/scripts/script-2-2a102aaa.js"),
+  import("./static/scripts/script-3-cc8afa9d.js"),
+  import("./static/scripts/script-4-7e107d7e.js"),
+  import("./static/scripts/script-5-5ef560a8.js"),
+  import("./static/scripts/script-6-5f71ef20.js")
+]);
+await import("./static/scripts/script-7-7ca62114.js");
